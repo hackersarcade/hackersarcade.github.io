2c9a0bda3986caef164dbd82ad0f91593e044e0b
vxsparsh <sparshanand752@gmailc.com>
2024-09-28 14:02:28 +0530

    updated

---
+++ assets/js/typed.js
@@ -0,0 +1,12 @@
+var typed = new Typed('#head', {
+    strings: ["Welcome to Hackers Arcade"],
+    typeSpeed: 20,
+  });
+  var typed2 = new Typed('#subhead', {
+    strings: ["Cybersecurity Community", "CTF Hosting Services", "Training and Workshop"],
+    typeSpeed: 20,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true,
+    smartBackspace: true 
+});
